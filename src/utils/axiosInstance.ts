965f10cb389f58070488c7020f27ccacb169355e
Hammad Ali <212019577+hammad-ali-next@users.noreply.github.com>
2025-06-03 02:17:36 +0500

    change of base url

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:8000",
-    // baseURL: "https://fast-api-first.vercel.app/",
+    // baseURL: "http://127.0.0.1:8000",
+    baseURL: "https://fast-api-first.vercel.app/",
     withCredentials: true,
 });
 
